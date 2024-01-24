be839f435bf95fdd30753123cf4cd3d6d9bd691c
Muhammad Anas Raza <memanasraza@gmail.com>
2024-01-24 17:51:29 -0500

    add numpy

--- k3_node/ops/numpy.py
+++ k3_node/ops/numpy.py
@@ -1,17 +1,5 @@
 from keras import backend
-
-
-if backend.backend() == "tensorflow":
-    import tensorflow as tf
-elif backend.backend() == "torch":
-    import torch
-elif backend.backend() == "jax":
-    import jax.numpy as jnp
-elif backend.backend() == "numpy":
-    import numpy as np
-else:
-    raise NotImplementedError
-
+from k3_node.utils.backend_import import *
 
 def polyval(p, x):
     if backend.backend() == "tensorflow":
