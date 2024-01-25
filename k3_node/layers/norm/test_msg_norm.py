49a23c44f44466c0577aa6e82505e53367089cbd
Muhammad Anas Raza <memanasraza@gmail.com>
2024-01-25 11:36:26 -0500

    add test for msg_norm

---
+++ k3_node/layers/norm/test_msg_norm.py
@@ -0,0 +1,16 @@
+from keras import ops, random
+
+from k3_node.layers import MessageNorm
+
+
+def test_message_norm():
+    norm = MessageNorm(learn_scale=True)
+    x = random.normal((100, 16))
+    msg = random.normal((100, 16))
+    out = norm((x, msg))
+    assert ops.shape(out) == (100, 16)
+
+
+    norm = MessageNorm(learn_scale=False)
+    out = norm((x, msg))
+    assert ops.shape(out) == (100, 16)
