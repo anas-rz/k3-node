ce27023d83b9c3e8fe5e8ae921e591d8a13c908c
Muhammad Anas Raza <memanasraza@gmail.com>
2024-01-26 11:35:03 -0500

    clean tests

--- k3_node/layers/attention/test_attention.py
+++ k3_node/layers/attention/test_attention.py
@@ -3,10 +3,13 @@ from keras import ops, random
 
 from k3_node.layers import PerformerAttention
 
-
-def test_performer_attention():
-    x = random.normal((1, 4, 16))
-    mask = ops.ones([1, 4])
-    attn = PerformerAttention(channels=16, heads=4)
+@pytest.mark.parametrize("in_channels", [32, 64])
+@pytest.mark.parametrize("out_channels", [32, 64])
+@pytest.mark.parametrize("heads", [4, 8])
+@pytest.mark.parametrize("num_nodes", [4, 8])
+def test_performer_attention(num_nodes, in_channels, out_channels, heads):
+    x = random.normal((1, num_nodes, in_channels))
+    mask = ops.ones([1, num_nodes])
+    attn = PerformerAttention(channels=out_channels, heads=heads)
     out = attn(x, mask)
-    assert out.shape == (1, 4, 16)
+    assert out.shape == (1, num_nodes, out_channels)
--- k3_node/layers/norm/test_norm.py
+++ k3_node/layers/norm/test_norm.py
@@ -1,6 +1,7 @@
+import numpy as np
 from keras import ops, random
 
-from k3_node.layers import MessageNorm
+from k3_node.layers import MessageNorm, MeanSubtractionNorm
 
 
 def test_message_norm():
@@ -13,3 +14,13 @@ def test_message_norm():
     norm = MessageNorm(learn_scale=False)
     out = norm((x, msg))
     assert ops.shape(out) == (100, 16)
+
+
+def test_mean_subtraction_norm():
+    x = random.normal((6, 16))
+
+    norm = MeanSubtractionNorm()
+
+    out = norm(x)
+    assert ops.shape(out) == (6, 16)
+    assert np.allclose(ops.mean(out), ops.convert_to_numpy(0.0), atol=1e-6)
