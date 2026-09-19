18671e56e140136bb07a41616a3d901d599165f2
Muhammad Anas Raza <memanasraza@gmail.com>
2026-09-19 01:28:09 -0400

    add polynormer

---
+++ k3_node/layers/attention/polynormer.py
@@ -0,0 +1,142 @@
+import keras
+from keras import ops
+
+
+class PolynormerAttention(keras.layers.Layer):
+    def __init__(
+        self,
+        channels,
+        heads,
+        head_channels=64,
+        beta=0.9,
+        qkv_bias=False,
+        qk_shared=True,
+        dropout=0.0,
+        **kwargs,
+    ):
+        super().__init__(**kwargs)
+
+        self.channels = channels
+        self.heads = heads
+        self.head_channels = head_channels
+        self.beta = beta
+        self.qk_shared = qk_shared
+
+        inner_channels = heads * head_channels
+
+        self.h_lins = keras.layers.Dense(inner_channels)
+
+        if not qk_shared:
+            self.q = keras.layers.Dense(
+                inner_channels,
+                use_bias=qkv_bias,
+            )
+
+        self.k = keras.layers.Dense(
+            inner_channels,
+            use_bias=qkv_bias,
+        )
+
+        self.v = keras.layers.Dense(
+            inner_channels,
+            use_bias=qkv_bias,
+        )
+
+        self.lns = keras.layers.LayerNormalization(epsilon=1e-5)
+        self.lin_out = keras.layers.Dense(inner_channels)
+        self.dropout = keras.layers.Dropout(dropout)
+
+    def call(self, x, mask=None, training=None):
+        shape = ops.shape(x)
+
+        B = shape[0]
+        N = shape[1]
+
+        h = self.h_lins(x)
+
+        k = self.k(x)
+        k = ops.sigmoid(k)
+        k = ops.reshape(
+            k,
+            (B, N, self.head_channels, self.heads),
+        )
+
+        if self.qk_shared:
+            q = k
+        else:
+            q = ops.sigmoid(self.q(x))
+            q = ops.reshape(
+                q,
+                (B, N, self.head_channels, self.heads),
+            )
+
+        v = self.v(x)
+        v = ops.reshape(
+            v,
+            (B, N, self.head_channels, self.heads),
+        )
+
+        if mask is not None:
+            mask = ops.expand_dims(mask, axis=-1)
+            mask = ops.expand_dims(mask, axis=-1)
+            v = v * ops.cast(mask, v.dtype)
+
+        # numerator
+        kv = ops.einsum(
+            "bndh,bnmh->bdmh",
+            k,
+            v,
+        )
+
+        num = ops.einsum(
+            "bndh,bdmh->bnmh",
+            q,
+            kv,
+        )
+
+        # denominator
+        k_sum = ops.einsum(
+            "bndh->bdh",
+            k,
+        )
+
+        den = ops.einsum(
+            "bndh,bdh->bnh",
+            q,
+            k_sum,
+        )
+
+        den = ops.expand_dims(den, axis=2)
+
+        x = num / (den + 1e-6)
+
+        x = ops.reshape(
+            x,
+            (B, N, self.head_channels * self.heads),
+        )
+
+        x = self.lns(x)
+        x = x * (h + self.beta)
+
+        x = self.lin_out(x)
+        x = ops.relu(x)
+
+        x = self.dropout(
+            x,
+            training=training,
+        )
+
+        return x
+
+    def get_config(self):
+        config = super().get_config()
+        config.update(
+            {
+                "channels": self.channels,
+                "heads": self.heads,
+                "head_channels": self.head_channels,
+                "beta": self.beta,
+                "qk_shared": self.qk_shared,
+            }
+        )
+        return config
--- k3_node/layers/attention/test_attention.py
+++ k3_node/layers/attention/test_attention.py
@@ -1,7 +1,7 @@
 import pytest
 from keras import ops, random
 
-from k3_node.layers import PerformerAttention
+from k3_node.layers import PerformerAttention, PolynormerAttention
 
 
 @pytest.mark.parametrize("in_channels", [32, 64])
@@ -14,3 +14,23 @@ def test_performer_attention(num_nodes, in_channels, out_channels, heads):
     attn = PerformerAttention(channels=out_channels, heads=heads)
     out = attn(x, mask)
     assert out.shape == (1, num_nodes, out_channels)
+
+
+
+@pytest.mark.parametrize("in_channels", [32, 64])
+@pytest.mark.parametrize("heads", [4, 8])
+@pytest.mark.parametrize("num_nodes", [4, 8])
+def test_polynormer_attention(num_nodes, in_channels, heads):
+
+    x = random.normal((1, num_nodes, in_channels))
+
+    mask = ops.ones([1, num_nodes])
+
+    attn = PolynormerAttention(
+        channels=in_channels,
+        heads=heads,
+    )
+
+    out = attn(x, mask)
+
+    assert out.shape == (1, num_nodes, heads * 64)
